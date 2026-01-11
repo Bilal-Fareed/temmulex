553f94acdc2744fcae6549566734bdcba79c9bf8
Bilal-Fareed <bilalfareed187@gmail.com>
2026-01-12 01:17:59 +0500

    signing client flow verified

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -24,7 +24,7 @@ const signupSchema = vine.object({
     first_name: vine.string().minLength(2).maxLength(100),
     last_name: vine.string().minLength(2).maxLength(100),
     country: vine.string().minLength(2).maxLength(100),
-    dob: vine.date(),
+    dob: vine.date({ formats: ['YYYY/DD/MM', 'x'] }),
     phone: vine.string().minLength(2).maxLength(50),
     languages: vine.array(vine.string()).optional().requiredWhen('user_type', '=', 'freelancer'),
     location: vine.object({ lat: vine.number(), lng: vine.number() }).optional().requiredWhen('user_type', '=', 'freelancer'),
