13fe59f6714cb3ddbb709cc7fd171aaa45b11a2c
Bilal-Fareed <bilalfareed187@gmail.com>
2026-01-16 00:46:03 +0500

    updated database connection

--- infra/db.js
+++ infra/db.js
@@ -8,11 +8,11 @@ const initializeDB = async () => {
 	try {
 		const pool = new Pool({
 			host: process.env.DB_HOST,
-			port: Number(process.env.DB_PORT),
+			// port: Number(process.env.DB_PORT),
 			user: process.env.DB_USER,
 			password: process.env.DB_PASSWORD,
 			database: process.env.DB_NAME,
-			ssl: process.env.DB_SSL === "true",
+			// ssl: process.env.DB_SSL === "true",
 		});
 
 		dbInstance = drizzle(pool, {
